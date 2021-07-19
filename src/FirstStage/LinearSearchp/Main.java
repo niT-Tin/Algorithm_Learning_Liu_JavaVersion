38053edcc1e0d37f1b7d769e5794e34900a39f8d
liuzehao <1437070389@qq.com>
2021-07-19 10:33:22 +0800

    Test LinearSearch Class

---
+++ src/FirstStage/LinearSearchp/Main.java
@@ -0,0 +1,9 @@
+package FirstStage.LinearSearchp;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] data = {24, 18, 12, 9, 16, 66, 32, 4};
+        // LinearSearch s = new LinearSearch();
+        System.out.println(LinearSearch.SearchV1(data, 16));
+    }
+}
